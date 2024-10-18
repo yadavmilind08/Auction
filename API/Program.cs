83bf27618e4d3c1391fb17abc3c4c4dd4110049a
Milind Yadav <yadavmilind08@gmail.com>
2024-10-18 22:32:27 +0530

    Updated request pipeline

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,13 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,7 +19,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors(builder=> builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();  
 
 app.MapControllers();
 
